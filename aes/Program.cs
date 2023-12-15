02d17cf79779e5d33c82611763004ed7b6d3efd2
ahmedbastawisi <ahmedbastawisi@outlook.com>
2023-12-15 14:25:46 +0200

    minor refactor

--- aes/Program.cs
+++ aes/Program.cs
@@ -1,47 +1,49 @@
 ﻿using System.Security.Cryptography;
 
-string original = "hello, world!";
+var original = "hello, world!";
 
-// Create a new instance of the Aes
-// class.  This generates a new key and initialization
-// vector (IV).
-using (Aes aes = Aes.Create())
+// Create a new instance of the Aes class.
+// This generates a new key and initialization vector (IV).
+using (var aes = Aes.Create())
 {
     // Display the aes info.
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
     Console.WriteLine(aes.ToString() + Environment.NewLine);
+    Console.ResetColor();
     Console.WriteLine("Key:        {0}", Convert.ToBase64String(aes.Key));
     Console.WriteLine("Iv:         {0}", Convert.ToBase64String(aes.IV));
     Console.WriteLine("KeySize:    {0}", aes.KeySize);
     Console.WriteLine("BlockSize:  {0}", aes.BlockSize);
 
-    // Encrypt the text to base64 string.
+    // Encrypt the plain text to encrypted base64 string.
     var encrypted = EncryptString(original, aes.Key, aes.IV);
 
-    // Convert encrypted text to an array of bytes
-    var encryptedbytes = Convert.FromBase64String(encrypted);
-
-    // Decrypt the bytes to a string.
-    string decrypted = DecryptString(encryptedbytes, aes.Key, aes.IV);
+    // Decrypt the encrypted base64 string back to plain text.
+    var decrypted = DecryptString(encrypted, aes.Key, aes.IV);
 
     //Display the original data and the decrypted data.
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Original:   {0}", original);
+    Console.ResetColor();
     Console.WriteLine("Encrypted:  {0}", encrypted);
+    Console.ForegroundColor = ConsoleColor.Green;
     Console.WriteLine("Decrypted:  {0}", decrypted);
+    Console.ResetColor();
+
 }
 
-static string EncryptString(string plainText, byte[] Key, byte[] IV)
+static string EncryptString(string plaintext, byte[] Key, byte[] IV)
 {
     byte[] encrypted;
 
-    // Create an Aes object
-    // with the specified key and IV.
-    using (Aes aes = Aes.Create())
+    // Create an Aes object with the specified key and IV.
+    using (var aes = Aes.Create())
     {
         aes.Key = Key;
         aes.IV = IV;
 
         // Create an encryptor to perform the stream transform.
-        ICryptoTransform encryptor = aes.CreateEncryptor();
+        var encryptor = aes.CreateEncryptor();
 
         // Create the streams used for encryption.
         using var ms = new MemoryStream();
@@ -49,34 +51,36 @@ static string EncryptString(string plainText, byte[] Key, byte[] IV)
         using (var sw = new StreamWriter(cs))
         {
             //Write all data to the stream.
-            sw.Write(plainText);
+            sw.Write(plaintext);
         }
         encrypted = ms.ToArray();
     }
-
     // Return the encrypted bytes from the memory stream as base64 string.
     return Convert.ToBase64String(encrypted);
 }
 
-static string DecryptString(byte[] ciphertext, byte[] Key, byte[] IV)
+static string DecryptString(string ciphertext, byte[] Key, byte[] IV)
 {
     string plaintext = null;
 
     // Create an Aes object with the specified key and IV.
-    using (Aes aes = Aes.Create())
+    using (var aes = Aes.Create())
     {
         aes.Key = Key;
         aes.IV = IV;
 
+        // Convert cipher text to an array of bytes.
+        var encrypted = Convert.FromBase64String(ciphertext);
+
         // Create a decryptor to perform the stream transform.
-        ICryptoTransform decryptor = aes.CreateDecryptor();
+        var decryptor = aes.CreateDecryptor();
 
         // Create the streams used for decryption.
-        using var ms = new MemoryStream(ciphertext);
+        using var ms = new MemoryStream(encrypted);
         using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
         using var sr = new StreamReader(cs);
 
-        // Read the decrypted bytes from the decrypting stream
+        // Read the decrypted bytes from the decrypting stream.
         plaintext = sr.ReadToEnd();
     }
     return plaintext;
